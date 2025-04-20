3d542ab382234f28021d6d7056eec35497376eb2
DoIKnowYou-DS <ryota.doi@datumstudio.jp>
2025-04-20 12:45:20 +0900

    feat: glue job

--- glue_training/module/aws/glue/main.tf
+++ glue_training/module/aws/glue/main.tf
@@ -9,3 +9,19 @@ resource "aws_glue_crawler" "crawler_raw" {
 
   table_prefix = var.raw_prefix
 }
+
+resource "aws_glue_job" "glue_job" {
+  name = "${var.pj_name}-glue-job"
+  role_arn = aws_iam_role.glue_role.arn
+  command {
+    script_location = "s3://${var.s3_bucket}/script/glue_job.py"
+  }
+
+  default_arguments = {
+    "--TempDir"      = "s3://${var.s3_bucket}/tmp/"
+    "--output_path"  = "s3://${var.s3_bucket}/processed/"
+    "--database"     = aws_glue_catalog_database.etl_db.name
+    "--table_name"   = "${var.raw_prefix}raw"
+  }
+
+}
--- glue_training/module/aws/s3/main.tf
+++ glue_training/module/aws/s3/main.tf
@@ -14,8 +14,20 @@ resource "aws_s3_object" "processed_folder" {
   key    = "processed/"
 }
 
+# glue job のスクリプト置き場
+resource "aws_s3_object" "scripts_folder" {
+  bucket = aws_s3_bucket.bucket.id
+  key    = "script/"
+}
+
 resource "aws_s3_object" "csv" {
   bucket = aws_s3_bucket.bucket.id
   key = "raw/sample_users.csv"
   source = "${path.module}/sample_users.csv"
+}
+
+resource "aws_s3_object" "job" {
+  bucket = aws_s3_bucket.bucket.id
+  key = "script/glue_job.py"
+  source = "${path.module}/glue_job.py"
 }
