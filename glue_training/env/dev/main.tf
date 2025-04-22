9a22fd8e2acd09201520c24f39a8fba227e23ed1
DoIKnowYou-DS <ryota.doi@datumstudio.jp>
2025-04-22 11:37:45 +0900

    fix: directory fix

--- glue_training/env/dev/main.tf
+++ glue_training/env/dev/main.tf
@@ -1,11 +1,11 @@
 module "s3" {
-  source            = "../module/aws/s3"
+  source            = "../../module/aws/s3"
   pj_name           = local.common.pj_name
   env_name          = local.common.env_name
 }
 
 module "glue" {
-  source            = "../module/aws/glue"
+  source            = "../../module/aws/glue"
   pj_name           = local.common.pj_name
   env_name          = local.common.env_name
   raw_prefix        = local.glue.raw_prefix
